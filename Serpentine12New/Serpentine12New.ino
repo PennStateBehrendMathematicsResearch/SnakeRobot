8514fca04ace979bda89326c70428d0b49fbedd4
picode98 <saaman1377@gmail.com>
2017-10-05 19:16:25 -0400

    Implement equivalent remote control set as SerpentineRev3, improve documentation, and other minor fixes
    Minor fixes: 3.14159 replaced with M_PI, waveOffsetMultiplier replaced
    with boolean reverseDirection, change wave generation function from
    cosine to sine, changed int literal to double literal

--- Serpentine12New/Serpentine12New.ino
+++ Serpentine12New/Serpentine12New.ino
@@ -3,24 +3,10 @@ Remote control file for serpentine motion
 of a snake robot with 12 servos
 */
 
-#include <Servo.h> 
+#include <Servo.h>
 
 const unsigned short NUM_SERVOS = 12;
 Servo robotServos[NUM_SERVOS];
-
-// Define servo objects for the snake segments
-/* Servo s1; 
-Servo s2;
-Servo s3;
-Servo s4; 
-Servo s5;
-Servo s6;
-Servo s7;
-Servo s8;
-Servo s9; 
-Servo s10;
-Servo s11;
-Servo s12; */
   
 // Define variables
 int forwardPin = 14;  // Remote control movement pins
@@ -33,28 +19,28 @@ int reverseVal = 0;
 int rightVal = 0;
 int leftVal = 0;
 
-// int counter = 0; // Loop counter variable
 float lag = .5236; // Phase lag between segments
 float frequency = 0.35; // Oscillation frequency of segments.
 int amplitude = 35; // Amplitude of the serpentine motion of the snake
 int rightOffset = 7; // Right turn offset
 int leftOffset = -7; // Left turn offset
-// int offset = 6; // Variable to correct servos that are not exactly centered
-// int delayTime = 5; // Delay between limb movements
 int startPause = 5000;  // Delay time to position robot
-// int test = -3; // Test varialble takes values from -6 to +5
 
-float forwardAngle = 93.0;
-float centerAngle = forwardAngle;
-float currentCenterAngle = forwardAngle;
-float waveOffsetMultiplier = 1.0;
-float waveValue = 0.0;
+float forwardAngle = 93.0; // Center servo angle for forward or reverse motion (without turning)
+float centerAngle = forwardAngle; // Center angle set point
+float currentCenterAngle = forwardAngle; // Current value of the center angle
+bool reverseDirection = false; // Boolean flag to store whether or not the robot is moving in a reverse direction
+float waveValue = 0.0; // Current base value for the wave generator (sine) function
 bool runningWave = false,
      runningWavePrevious = runningWave;
-float turnRampRate = 3;
+float turnRampRate = 3; // Maximum rate at which turn offsets are ramped (degrees per second)
 unsigned long currentTimeStamp,
               lastTimeStamp;
 
+// This method returns the result of constraining value to the range
+// between lowerBound and upperBound by returning lowerBound if value
+// is less than lowerBound, upperBound if value is greater than
+// upperBound, and value otherwise.
 float coerceToRange(float lowerBound, float upperBound, float value)
 {
   if(value > upperBound)
@@ -73,19 +59,20 @@ void setup()
 { 
   // Serial.begin(115200);
   
-// Set movement pins as inputs
+  // Set movement pins as inputs
   pinMode(forwardPin, INPUT);
   pinMode(reversePin, INPUT);
   pinMode(rightPin, INPUT);
   pinMode(leftPin, INPUT);
   
-// Set movement pins to low
+  // Set movement pins to low
   digitalWrite(forwardPin, LOW);
   digitalWrite(reversePin, LOW);
   digitalWrite(rightPin, LOW);
   digitalWrite(leftPin, LOW);
   
-// Attach segments to pins
+  // Attach segment servos to pins and initialize them to their
+  // starting positions
 
   centerAngle = forwardAngle;
   currentCenterAngle = forwardAngle;
@@ -93,28 +80,12 @@ void setup()
   for(int i = 0; i < NUM_SERVOS; i++)
   {
     robotServos[i].attach(13 - i);
-    robotServos[i].write(centerAngle+amplitude*cos(i * lag));
+    robotServos[i].write(centerAngle+amplitude*sin(i * lag));
     // Serial.print("Started servo connected to " + String(13 - i) + " with " + String(centerAngle+amplitude*cos(i * lag)) + ".\n"); 
   }
 
-
- 
-// Put snake in starting position
-   /* s1.write(93+amplitude*cos(5*lag));
-   s2.write(93+amplitude*cos(4*lag)); 
-   s3.write(93+amplitude*cos(3*lag));
-   s4.write(93+amplitude*cos(2*lag));
-   s5.write(93+amplitude*cos(1*lag));
-   s6.write(93+amplitude*cos(0*lag));
-   s7.write(93+amplitude*cos(-1*lag));
-   s8.write(93+amplitude*cos(-2*lag));
-   s9.write(93+amplitude*cos(-3*lag));
-   s10.write(93+amplitude*cos(-4*lag));
-   s11.write(93+amplitude*cos(-5*lag));
-   s12.write(93+amplitude*cos(-6*lag)); */
- 
-   
-  delay(startPause);  // Pause to position robot
+  // Pause to position robot
+  delay(startPause);
 } 
   
   
@@ -125,192 +96,75 @@ void loop()
     reverseVal = digitalRead(reversePin);
     rightVal = digitalRead(rightPin);
     leftVal = digitalRead(leftPin);
-    
-  // Forward motion
-    if (forwardVal == HIGH){
-      /* for(counter = 0; counter < 360; counter += 1)  {
-        delay(delayTime);
-        s1.write(93+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      } */
-  
-      runningWave = true;
-      centerAngle = forwardAngle;
-      waveOffsetMultiplier = 1.0;
-    }
-  // Reverse motion
-    else if (reverseVal == HIGH){
-      /* for(counter = 360; counter > 0; counter -= 1)  {
-        delay(delayTime);
-        s1.write(93+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      } */
-  
-      runningWave = true;
-      centerAngle = forwardAngle;
-      waveOffsetMultiplier = -1.0;
-    }
-  // Right turn
-    else if (rightVal == HIGH){
-  // Ramp up turn offset
-      /* for(counter = 0; counter < 10; counter += 1)  {
-        delay(delayTime);
-        s1.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+.1*counter*rightOffset+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      }
-  // Continue right turn
-      for(counter = 11; counter < 350; counter += 1)  {
-        delay(delayTime);
-        s1.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+rightOffset+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      }    
-  // Ramp down turn offset
-      for(counter = 350; counter < 360; counter += 1)  {
-        delay(delayTime);
-        s1.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+.1*(360-counter)*rightOffset+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      } */
-  
+
+    // Right turn
+    if (rightVal == HIGH){
       runningWave = true;
       centerAngle = forwardAngle + rightOffset;
       // waveOffsetMultiplier = 1.0;
       // Serial.println("Turning right...");
     }
-  // Left turn
+    // Left turn
     else if (leftVal == HIGH){
-  // Ramp up turn offset
-      /* for(counter = 0; counter < 10; counter += 1)  {
-        delay(delayTime);
-        s1.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+.1*counter*leftOffset+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      }  
-  // Continue left turn
-      for(counter = 11; counter < 350; counter += 1)  {
-        delay(delayTime);
-        s1.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+leftOffset+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      }    
-  // Ramp down turn offset
-      for(counter = 350; counter < 360; counter += 1)  {
-        delay(delayTime);
-        s1.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        s2.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180+4*lag));
-        s3.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180+3*lag));
-        s4.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180+2*lag));
-        s5.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180+1*lag));
-        s6.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180+0*lag));    
-        s7.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180-1*lag));
-        s8.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180-2*lag));    
-        s9.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180-3*lag));
-        s10.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180-4*lag));
-        s11.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180-5*lag));    
-        s12.write(93+.1*(360-counter)*leftOffset+amplitude*cos(frequency*counter*3.14159/180-6*lag));     
-      } */
-  
       runningWave = true;
       centerAngle = forwardAngle + leftOffset;
       // Serial.println("Turning left...");
       // waveOffsetMultiplier = 1.0;
     }
+    // Straight movement
     else
+    {
+      centerAngle = forwardAngle;
+    }
+    
+    // Forward motion
+    if (forwardVal == HIGH){
+      runningWave = true;
+      reverseDirection = false;
+    }
+    // Reverse motion
+    else if (reverseVal == HIGH){
+      runningWave = true;
+      reverseDirection = true;
+    }
+    // Stop if a turn is not being made
+    else if(leftVal == LOW && rightVal == LOW)
     {
       runningWave = false;
     }
 
+    // If the robot has just started running
     if(runningWave && !runningWavePrevious)
     {
+      // Reset the last time stamp to the current millisecond timer value
       lastTimeStamp = millis();
     }
 
     runningWavePrevious = runningWave;
-  
+
+    // If the robot is running
     if(runningWave)
     {
+      // Calculate the elapsed time 
       currentTimeStamp = millis();
       float elapsedTime = (currentTimeStamp - lastTimeStamp) / 1000.0;
       lastTimeStamp = currentTimeStamp;
 
-      waveValue += ((-2 * 3.14159 * frequency) * (elapsedTime * waveOffsetMultiplier));
+      // Calculate the new base wave generator value
+      waveValue += ((-2.0 * M_PI * frequency)* (elapsedTime * (reverseDirection ? -1.0 : 1.0)));
 
+      // Calculate the maximum change in the current center angle
       float maxAngleChange = turnRampRate * elapsedTime;
 
+      // Calculate the new center angle
       currentCenterAngle = coerceToRange(currentCenterAngle - maxAngleChange, currentCenterAngle + maxAngleChange, centerAngle);
       // Serial.println("Current center angle: " + String(currentCenterAngle));
-      
+
+      // Loop to update robot servos
       for(int i = 0; i < NUM_SERVOS; i++)
       {
         // s1.write(currentAngle + amplitude*cos(frequency*counter*3.14159/180+5*lag));
-        robotServos[i].write(currentCenterAngle + amplitude*cos(waveValue + (i * lag)));
+        robotServos[i].write(currentCenterAngle + amplitude*sin(waveValue + (i * lag)));
       }
     }
 }
